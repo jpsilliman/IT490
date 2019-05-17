efdf82c954d09dd4a4d67f6a578cbd4c233431ad
tjwags <tjwags182@gmail.com>
2019-05-16 23:15:14 -0400

    phpbb--final

---
+++ phpbb/config.php
@@ -0,0 +1,19 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = '192.168.3.33';
+$dbport = '';
+$dbname = 'phpbb';
+$dbuser = 'phpbb_user';
+$dbpasswd = 'pass';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+ @define('DEBUG_CONTAINER', true);
+ @define('DEBUG', true);
+ //@define('DEBUG_EXTRA, true);
